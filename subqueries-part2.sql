cbf38f91bd9edebe2f78e95905396c20b5b32705
Harsh Saini <harshsaini132159@gmail.com>
2026-06-26 12:21:58 +0530

    corelated subqueries done

--- subqueries-part2.sql
+++ subqueries-part2.sql
@@ -80,3 +80,66 @@ WHERE salary =
     FROM employees_join e2
     WHERE e2.department_id = e1.department_id
 );
+
+-- Find employees whose salary is below their department average.
+
+SELECT emp_name,
+       department_id,
+       salary
+FROM employees_join e1
+WHERE salary <
+(
+    SELECT avg(salary)
+    FROM employees_join e2
+    WHERE e2.department_id = e1.department_id
+);
+
+-- Find departments that have no employees.
+
+SELECT d.department_name
+FROM employees_join e
+right JOIN departments d
+ON e.department_id = d.department_id
+WHERE e.emp_id IS NULL;
+
+-- subquery
+
+SELECT department_name
+FROM departments
+WHERE department_id NOT IN (
+    SELECT DISTINCT department_id
+    FROM employees_join
+    WHERE department_id IS NOT NULL
+);
+
+-- Try using a subquery instead of a JOIN.
+
+-- Find employees working in departments that have more than 2 employees.
+
+select emp_name
+from employees_join
+where department_id IN 
+( select department_id
+from employees_join
+group by department_id
+having count(*) > 2
+);
+
+
+-- Find the department(s) with the highest average salary.
+
+select d.department_name, avg(e.salary) as AVG_SAL
+from employees_join e
+inner join departments d
+on d.department_id = e.department_id
+group by d.department_name
+order by AVG_SAL DESC
+limit 1;
+
+
+
+
+
+
+
+-- (Not just one row—think about ties.)
